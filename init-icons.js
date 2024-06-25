d501adad97a0931e97586bee6d71efdb18e7f521
HT <45606634+glenthemes@users.noreply.github.com>
2024-06-24 20:48:03 -0700

    2021 – update broken fetch link

--- init-icons.js
+++ init-icons.js
@@ -1,30 +1,46 @@
-document.addEventListener("DOMContentLoaded",() => { 
-    initBasicons();
-});
+/*------------------------------------------------------
 
-function initBasicons(){
-	document.querySelectorAll("[class*='bsc-']").forEach(bsc => {	
-		// identify bsc icon string
-		var bsc_name = bsc.getAttribute("class").split("bsc-").pop();
+   Basicons (c) Solomon Omojola 2021
 
-		if(bsc_name.indexOf(" ") > -1){
-			bsc_name = bsc_name.substring(0, bsc_name.lastIndexOf(" "));
-		}
-		
-		// match icon name to icon svg url
-		fetch("//basicons.xyz/raw_icons/" + bsc_name + ".svg")
-		.then(svgspec => {
-		  return svgspec.text();
+   * SITE: dub.sh/basicons-2021
+   * LIST (NAMES ONLY): git.new/basicons-2021-list
+   * USAGE: git.new/basicons-2021
+
+-------------------------------------------------------*/
+
+window.basicons = function(){
+	let initBasicons = () => {
+		document.querySelectorAll("[class*='bsc-']")?.forEach(bsc => {
+			// identify bsc icon string
+			let bsc_name = bsc.getAttribute("class").split("bsc-").pop();
+			if(bsc_name.indexOf(" ") > -1){
+				bsc_name = bsc_name.substring(0, bsc_name.lastIndexOf(" "));
+			}
+
+			// match icon name to icon svg url
+			fetch("//glenthemes.github.io/basicons/2021/svg/" + bsc_name + ".svg")
+			.then(svgspec => {
+			  return svgspec.text();
+			})
+			.then(svgspec => {
+			  bsc.innerHTML = svgspec;
+			  if(bsc.querySelectorAll("meta[name='viewport']").length){
+				bsc.innerHTML = "";
+			  }
+			}).catch(error => {
+				console.error(error)
+			})
+
+			// add .basicons class at the start of each
+			let cur_classes = bsc.getAttribute("class");
+			bsc.setAttribute("class","basicons " + cur_classes)
 		})
-		.then(svgspec => {
-		  bsc.innerHTML = svgspec;
-		  if(bsc.querySelectorAll("meta[name='viewport']").length){
-		    bsc.innerHTML = "";
-		  }
-		});
 		
-		// add .basicons class at the start of each
-		var cur_classes = bsc.getAttribute("class");
-		bsc.setAttribute("class","basicons " + cur_classes)
-	});
+	}
+	
+	document.readyState == "loading" ?
+    document.addEventListener("DOMContentLoaded", () => initBasicons()) :
+    initBasicons();
 }
+
+basicons();
